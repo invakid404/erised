3ea2f4408e4a4e44ed8e02dee6af96efd18dc895
invakid404 <splashgamingbg@gmail.com>
2021-03-03 17:12:22 +0200

    feat: add an example plugin

---
+++ test-widget/test.h
@@ -0,0 +1,27 @@
+//
+// Created by inva on 3/3/21.
+//
+
+#ifndef ERISED_TEST_H
+#define ERISED_TEST_H
+
+#include <widget/interface.h>
+
+#include <QObject>
+#include <QPushButton>
+#include <QtPlugin>
+
+class test : public QObject, erised::widget::widget_interface {
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "tk.erised.Erised.TestWidget" FILE "test-widget.json")
+    Q_INTERFACES(widget_interface)
+public:
+    QWidget* instantiate_widget() override {
+        auto* button = new QPushButton("Hello, world!");
+        connect(button, &QPushButton::released, [&]() { qDebug() << "Hello from the widget!"; });
+
+        return button;
+    }
+};
+
+#endif  // ERISED_TEST_H
